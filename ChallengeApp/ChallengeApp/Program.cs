d4582aab7afac53dedb9b79047660b5b166e0cd0
Krzysztof Lipiński <krislipa70@gmail.com>
2024-02-04 19:46:57 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string imie = "Krzysiu";
+int wiek = 20;
+bool mezczyzna = true;
+
+if (mezczyzna && wiek < 30)
+{
+    Console.WriteLine("Mężczyzna poniżej 30 lat");
+}
+else if (mezczyzna && wiek > 30)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (!mezczyzna && wiek < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (!mezczyzna && wiek == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
