e08d334608d0e5ee13bf44d20c2451b8f34534de
Krzysztof Lipiński <krislipa70@gmail.com>
2024-02-04 22:00:05 +0100

    Zad dzień 4 (poprawione)

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,20 @@
-﻿string imie = "Krzysiu";
-int wiek = 20;
-bool mezczyzna = true;
+﻿string name = "Krzysiu";
+int age = 20;
+bool man = true;
 
-if (mezczyzna && wiek < 30)
+if (man && age < 30)
 {
     Console.WriteLine("Mężczyzna poniżej 30 lat");
 }
-else if (mezczyzna && wiek > 30)
+else if (man && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
 }
-else if (!mezczyzna && wiek < 30)
+else if (!man && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (!mezczyzna && wiek == 30)
+else if (!man && age == 30)
 {
     Console.WriteLine("Ewa lat 30");
 }
